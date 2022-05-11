2c24d21a1bb02795aae485cf31fa1de046f095b4
snehasonwane <snehasonwane01@gmail.com>
2022-05-11 23:00:11 +0530

    [Manasi] Add: Peek element in stack

--- StackandQueue/Program.cs
+++ StackandQueue/Program.cs
@@ -1,18 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StackandQueue
+using StackandQueue;
+class Program
 {
     public static void Main(String[] args)
     {
         Console.WriteLine("Welcome In Stack and Queue Operation");
-
-    }
-    StackandQueue stack = new StackandQueue();
+        StackandQueue stack = new StackandQueue();
     Console.WriteLine("Display Stack Elements 1\n Peek And Pop Operation 2\n Deleting Top of the Elements Of Stack 3\n Checking Stack is Empty 4\n Display Queue Elements 5\n Delete Elements From Queues 6\n");
         bool check = true;
         while (check)
@@ -26,9 +20,11 @@ namespace StackandQueue
                     stack.Push(30);
                     stack.Push(56);
                     stack.Display();
+                case 2:
+                    stack.Peek();
                     break;
-        
-        }
+
+            }
        }
     }
 }
