b8beedf395b44a5303b86330868903764cd15249
snehasonwane <snehasonwane01@gmail.com>
2022-05-11 08:36:01 +0530

    [Manasi] Add: Push Element In stack

---
+++ StackandQueue/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackandQueue
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Welcome In Stack and Queue Operation");
+
+    }
+    StackandQueue stack = new StackandQueue();
+    Console.WriteLine("Display Stack Elements 1\n Peek And Pop Operation 2\n Deleting Top of the Elements Of Stack 3\n Checking Stack is Empty 4\n Display Queue Elements 5\n Delete Elements From Queues 6\n");
+        bool check = true;
+        while (check)
+        { 
+        Console.WriteLine("Enter the Above Option To Perform the Operation");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+        {
+        case 1: 
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
+                    stack.Display();
+                    break;
+        
+        }
+       }
+    }
+}
+
